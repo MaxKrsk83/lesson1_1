b8c4a74147ee9287f7b10c827154bc3a7f38284d
MaxKrsk83 <mcsimus.krsk@gmail.com>
2021-10-02 16:26:55 +0700

    дз1

--- src/main/java/HW1.java
+++ src/main/java/HW1.java
@@ -46,7 +46,7 @@ public class HW1 {
     public static boolean IsLeapYear(int year){
         if (year%4==0){
             if (year%100==0){
-                return year % 400 == 0;
+                return year % 400 != 0;
             }else {
                 return true;
             }
